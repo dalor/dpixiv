e6a7847428406d31019d8bd9ecb3efffd8e0cbfa
dalor <dalor@i.ua>
2018-11-24 14:07:03 +0200

    v 2.0.0

--- README.md
+++ README.md
@@ -10,17 +10,42 @@ from dpixiv import DPixivIllusts
 
 pix = DPixivIllusts('login', 'password') #Auth
 
-pictures = pix.recommender() #Get lists of ids
+pictures = pix.recommender() #Get lists of ids that recommended to user by pixiv
 # Has two parameters: (sample_illusts=None, count=100) - by default
-# sample_illusts - list of ids(str) which help you to find what you want
+# sample_illusts - id or list of ids which help you to find what you want
 # count - count of elements which you will get
 
-pictures_info = pix.illust_list(pictures) #Get list of information of pictures
+short_pictures_info = pix.illust_list(pictures) #Get list(or not) of short information of pictures (fast speed)
 # Has one parameter: (illusts) - required
-# illusts - list of ids of pictures
+# illusts - id or list of ids of pictures
 
-picture_urls = pix.urls(pictures) #Get list of urls with ids of pictures
-# Has one parameter: (illusts) - required
-# illusts - list of ids of pictures
+one_picture = picture[0]
+
+full_picture_info = pix.info(one_picture) #Get full information of picture (not so fast)
+# Has one parameter: (id) - required
+# id - id of picture
+
+#Use next method for a lot of pictures(much faster then one by one)
+full_pictures_info = pix.info_packs(picture) #Get list of full information of picture (medium speed)
+# Has one parameter: (id) - required
+# id - list of ids of pictures
+
+pictures = pix.similar(one_picture) #Get lists of ids of pictures that similar to picture with id (fast speed)
+# Has two parameters: (id, limit=10) - by default
+# id - id of picture
+# limit - max count of elements which you can get
+
+picture_urls = pix.bookmarks(pictures) #Get list of bookmarks of user
+# Has four parameters: (page=None, from_page=1, to_page=1000000, step_count=10) - by default
+# page - number of page of bookmarks to show one page
+# OR 
+# from_page - start page of list
+# to_page - final page of list
+# step_count - count pages to parse in one step(change for your task - can make your program faster)
+
+pix.auth() #Updating session or changing login and password 
+# Has two parameters: (login=None, password=None) - by default
+# login - if None uses default login
+# password - if None uses default password
 
 ```
--- dpixiv.py
+++ dpixiv.py
@@ -3,63 +3,75 @@ import json
 import aiohttp
 import asyncio
 
+post_key_search = re.compile(r'name\=\"post_key\" value\=\"([a-z0-9]+)\"')
+tt_search = re.compile(r'name\=\"tt\" value\=\"([a-z0-9]+)\"')
+reccomend_sample_search = re.compile(r'illustRecommendSampleIllust \= \"([0-9\,]+)\"\;')
+info_json_pattern = re.compile('\}\)\((\{token\:.+\})\)\;\<\/script\>')
+info_fix_json = re.compile(r'([\,\{]) ?(\w+):')
+
 class DPixivIllusts:
     def __init__(self, login, password):
-        self.login = login
-        self.password = password
+        self.__login = login
+        self.__password = password
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36'}
         self.tt = None
         self.cookies = {}
         self.auth()
     
-    def get(self, urls, params=None, ref=None, list=False):
-        async def fetch_for_sending(url, session, params=None, dict_=False):
-            if dict_:
-                async with session.get(url['url']) as response: #headers={'Referer': url['url']}
-                    return {'data': await response.text(), 'id': url['id'] if 'id' in url else None}
-            else:
-                async with session.get(url, params=params) as response:
-                    self.cookies.update(dict(response.cookies))
-                    return await response.text()
-        async def sending_one_or_more(url, params, ref, list):
-            headers = self.headers
-            if ref:
-                headers['Referer'] = ref
-            async with aiohttp.ClientSession(cookies=self.cookies, headers=headers) as session:
-                if list:
-                    return await asyncio.gather(*[asyncio.ensure_future(fetch_for_sending(url0, session, dict_=True)) for url0 in url])
-                else:
-                    return await fetch_for_sending(url, session, params)
-        return asyncio.new_event_loop().run_until_complete(sending_one_or_more(urls, params, ref, list))
+    async def __fetch_get(self, url, session, params=None, ref=None):
+        async with session.get(url, params=params, headers={'Referer': ref} if ref else None) as resp:
+            return await resp.text()
+            
+    async def __fetch_get_with_id(self, url, id_, session, params=None, ref=None):
+        async with session.get(url, params=params, headers={'Referer': ref} if ref else None) as resp:
+            return {id_: (await resp.text())}
+            
+    async def __fetch_post(self, url, session, data):
+        async with session.post(url, data=data) as resp:
+            return await resp.text()
     
-    def post(self, url, data):
-        async def post_one(url):
-            async with aiohttp.ClientSession(cookies=self.cookies) as session:
-                async with session.post(url, data=data) as response:
-                    self.cookies.update(dict(response.cookies))
-                    return await response.text()
-        return asyncio.get_event_loop().run_until_complete(post_one(url))
-        
+    async def __get(self, url, params=None, ref=None):
+        async with aiohttp.ClientSession(headers=self.headers, cookies=self.cookies) as session:
+            return await (self.__fetch_get(url, session, params, ref))
+            
+    async def __get_list_with_id_prepare(self, list_):
+        new_list_ = {}
+        async with aiohttp.ClientSession(headers=self.headers, cookies=self.cookies) as session:
+            for resp in (await asyncio.gather(*[asyncio.ensure_future(self.__fetch_get_with_id(url, id_, session)) for id_, url in list_.items()])):
+                new_list_.update(resp)
+        return new_list_
+    
+    def __get_list_with_id(self, list_):
+        return asyncio.new_event_loop().run_until_complete(self.__get_list_with_id_prepare(list_))   
+         
+    def get(self, url, params=None, ref=None):
+        return asyncio.new_event_loop().run_until_complete(self.__get(url, params, ref))
     
     def auth(self, login=None, password=None): #Use to set up all cookies use again to reauth
-        login = self.login if not login else login
-        password = self.password if not password else password
-        prepost_key = re.search(r'name\=\"post_key\" value\=\"([a-z0-9]+)\"', self.get('https://accounts.pixiv.net/login?lang=en&source=pc&view_type=page&ref=wwwtop_accounts_index'))
-        login_params = {
-            'password': password,
-            'pixiv_id': login,
-            'captcha': '',
-            'g_recaptcha_response': '',
-            'post_key': prepost_key[1] if prepost_key else None,
-            'ref': 'wwwtop_accounts_index',
-            'return_to': 'https://www.pixiv.net/',
-            'source': 'pc'
-        }
-        self.post('https://accounts.pixiv.net/api/login?lang=en', data=login_params)
-        prett = re.search(r'name\=\"tt\" value\=\"([a-z0-9]+)\"', self.get('https://www.pixiv.net'))
-        self.tt = prett[1] if prett else None
+        login = self.__login if not login else login
+        password = self.__password if not password else password
+        async def work_in_one_session():
+            async with aiohttp.ClientSession(headers=self.headers) as session:
+                login_page = await (self.__fetch_get('https://accounts.pixiv.net/login?lang=en&source=pc&view_type=page&ref=wwwtop_accounts_index', session))
+                prepost_key = post_key_search.search(login_page)
+                login_params = {
+                    'password': password,
+                    'pixiv_id': login,
+                    'captcha': '',
+                    'g_recaptcha_response': '',
+                    'post_key': prepost_key[1] if prepost_key else None,
+                    'ref': 'wwwtop_accounts_index',
+                    'return_to': 'https://www.pixiv.net/',
+                    'source': 'pc'
+                }
+                await (self.__fetch_post('https://accounts.pixiv.net/api/login?lang=en', session, data=login_params))
+                prett = tt_search.search(await (self.__fetch_get('https://www.pixiv.net', session)))
+                clear_cookies = { cookie.key:cookie.value for cookie in session.cookie_jar}
+                self.tt = prett[1] if prett else None
+                self.cookies = clear_cookies
+        asyncio.new_event_loop().run_until_complete(work_in_one_session())
 
-    def recommender(self, sample_illusts=None, count=100): #Return list of recommendations or None if error; sample_illusts - str of id or list of str of id; can be executed without parameters and show all recommendations
+    def recommender(self, sample_illusts=None, count=100): #Return list of recommendations or None if error; sample_illusts - id or list ids; can be executed without parameters and show all recommendations
         rec_params = {
             'type': 'illust',
             'num_recommendations': count,
@@ -70,34 +82,35 @@ class DPixivIllusts:
         else:
             rec_params.update({'sample_illusts': ','.join(sample_illusts) if type(sample_illusts) == list else sample_illusts})
         rec_resp = json.loads(self.get('http://www.pixiv.net/rpc/recommender.php', params=rec_params, ref='https://www.pixiv.net/discovery'))
-        return [str(i) for i in rec_resp['recommendations']] if 'recommendations' in rec_resp else None
+        return [i for i in rec_resp['recommendations']] if 'recommendations' in rec_resp else None
     
     def similar(self, id, limit=10):
-        sim_resp = json.loads(self.get('https://www.pixiv.net/ajax/illust/{}/recommend/init?limit={}'.format(id, str(limit))))
+        sim_resp = json.loads(self.get('https://www.pixiv.net/ajax/illust/{}/recommend/init?limit={}'.format(id, limit)))
         return [one['workId'] for one in sim_resp['body']['illusts']] if not sim_resp['error'] else None
     
-    def bookmarks(self, page=None, max_count=10):
-        rec_sample = re.compile(r'illustRecommendSampleIllust \= \"([0-9\,]+)\"\;')
+    def bookmarks(self, page=None, from_page=1, to_page=1000000, step_count=10): #Get bookmarks of user
         if page:
-            res_parse = rec_sample.search(self.get('https://www.pixiv.net/bookmark.php?p=' + str(page)))
+            res_parse = reccomend_sample_search.search(self.get('https://www.pixiv.net/bookmark.php?p={}'.format(page)))
             return res_parse[1].split(',') if res_parse else None
         else:
             results = []
-            pg = 1
+            if step_count > to_page: step_count = to_page
+            pg = from_page
             end = False
-            while not end:
-                prepared = self.get([{'url': 'https://www.pixiv.net/bookmark.php?p=' + str(i)} for i in range(pg, int(max_count) + pg)], list=True)
-                for one in prepared:
-                    res_parse = rec_sample.search(one['data'])
+            while not end and pg <= to_page:
+                prepared = self.__get_list_with_id({i: 'https://www.pixiv.net/bookmark.php?p={}'.format(i) for i in range(pg, step_count + pg)})
+                prepared_items = sorted(prepared.items())
+                for key, value in prepared_items:
+                    res_parse = reccomend_sample_search.search(value)
                     if res_parse:
                         results.extend(res_parse[1].split(','))
                     else:
                         end = True
                         break
-                pg += max_count
+                pg += step_count
             return results if results else None
                 
-    def illust_list(self, illusts): #Returns list of descriptions of illusts; illusts - list of str of id
+    def illust_list(self, illusts): #Returns list of descriptions of illusts; illusts - one id or list of ids
         list_params = {
             'exclude_muted_illusts': 1,
             'illust_ids': ','.join(illusts),
@@ -105,12 +118,17 @@ class DPixivIllusts:
             'tt': self.tt
         }
         return json.loads(self.get('https://www.pixiv.net/rpc/illust_list.php', params=list_params, ref='https://www.pixiv.net/discovery'))
-        
-    def info(self, id):
-        find_info = re.search('\}\)\((\{token\:.+\})\)\;\<\/script\>', self.get('https://www.pixiv.net/member_illust.php?mode=medium&illust_id=' + id))
-        return json.loads(re.sub(r'([\,\{ ])(\w+):', r'\1"\2":', find_info[1].replace(',}','}'))) if find_info else None
-        
-    def urls(self, illusts): #Returns list of urls with id
-        pattern_urls = re.compile(r'\"urls\"\:(\{.+\}),\"tags\"')
-        pages = self.get([{'url': 'https://www.pixiv.net/member_illust.php?mode=medium&illust_id=' + one, 'id': one} for one in illusts], list=True)
-        return [{'urls': json.loads(pattern_urls.search(page['data'])[1]), 'id': page['id']} for page in pages]
+    
+    def __parse_info(self, page):
+        find_json = info_json_pattern.search(page)
+        if find_json:
+            return json.loads(info_fix_json.sub(r'\1"\2":', find_json[1].replace(',}','}')))
+        else:
+             None
+    
+    def info(self, id): #Return all info; get one id
+        return self.__parse_info(self.get('https://www.pixiv.net/member_illust.php?mode=medium&illust_id={}'.format(id)))
+    
+    def info_packs(self, illusts): #Return all info; get list of ids
+        pages = self.__get_list_with_id({one: 'https://www.pixiv.net/member_illust.php?mode=medium&illust_id={}'.format(one) for one in illusts})
+        return [self.__parse_info(pages[ilust]) for ilust in illusts]
